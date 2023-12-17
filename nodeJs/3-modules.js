12bac1391d9bbb2ec8507e7efbb68b3f89570413
Muqtadir <muqtadir.mujahid02@gmail.com>
2023-12-17 20:25:58 +0600

    nodejs learning from freecodecamp

---
+++ nodeJs/3-modules.js
@@ -0,0 +1,10 @@
+//modules
+
+const names=require('./4-names')
+// console.log(names)
+const sayHi=require('./5-utils')
+const data=require('./6-alternative-flavor')
+require('./7-mind-grenade')
+// sayHi('Susan')
+// sayHi(names.john)
+// sayHi(names.peter)
