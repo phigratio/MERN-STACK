fe62c945b970e74b615a70d020113dad33fd176c
Muqtadir <muqtadir.mujahid02@gmail.com>
2024-01-15 20:57:28 +0600

    learning react and node at the same time

--- react/18 Fragments/learning-fragments/src/App.jsx
+++ react/18 Fragments/learning-fragments/src/App.jsx
@@ -4,29 +4,34 @@ import FoodItems from "./components/FoodItems";
 import ErrorMessage from "./components/ErrorMessage";
 import Container from "./components/Container";
 import FoodInput from "./components/FoodInput";
+import { useState } from "react";
 
 function App() {
   // let foodItems = [];
   let foodItems = ["Dal", "Green Vegetables", "Roti", "Salad"];
+  let textStateArr = useState("Food Item Entered By User");
+  let textStateVal = textStateArr[0];
+  let setTextState = textStateArr[1];
+  console.log(`Current Value of TextState:${textStateVal}`);
   // if (foodItems.length === 0) {
   //   return <h3>I am still hungry</h3>;
   // }
   const handleOnChange = (event) => {
     console.log(event.target.value);
+    setTextState(event.target.value);
   };
   return (
     <>
       <Container>
-        <Container>
-          <h1 className="food-heading"> Healthy Foods</h1>
-        </Container>
+        <h1 className="food-heading"> Healthy Foods</h1>
         <ErrorMessage items={foodItems}></ErrorMessage>
         <FoodInput handleOnChange={handleOnChange}></FoodInput>
+        <p>{textStateVal}</p>
         <FoodItems items={foodItems}></FoodItems>
       </Container>
-      <Container>
+      {/* <Container>
         <p>Above is the list of foods that are good for health.</p>
-      </Container>
+      </Container> */}
     </>
   );
 }
